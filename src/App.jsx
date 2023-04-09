da09a09de594493f3ef2dc280ea813938c62e76f
Tawfeeq Al-Mumen <almumen@gmail.com>
2023-04-10 00:01:18 +0300

    modify useProSidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
-import Topbar from './pages/global/Topbar';
-import {ColorModeContext, useMode} from './theme';
-import { CssBaseline,ThemeProvider } from '@mui/material';
-
+import Topbar from "./pages/global/Topbar";
+import Sidebar from "./pages/global/Sidebar";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/dashboard";
+import { useState } from "react";
 
 function App() {
-  const [theme,colorMode] = useMode();
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
-  <ColorModeContext.Provider value={colorMode}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <div className="app">
-        <main className='content'>
-          <Topbar/>
-        </main>
-      </div>
-     </ThemeProvider>
-  </ColorModeContext.Provider>
-
-  )
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <CssBaseline />
+          <div className="app">
+            <ProSidebarProvider>
+              <Sidebar isSidebar={isSidebar}>
+                <main className="content">
+                  <Topbar />
+                  <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                  </Routes>
+                </main>
+              </Sidebar>
+            </ProSidebarProvider>
+          </div>
+        </Router>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- src/pages/global/Topbar.jsx
+++ src/pages/global/Topbar.jsx
@@ -1,7 +1,7 @@
 import { Box,IconButton,useTheme } from "@mui/material"
 import { useContext } from "react"
 import { ColorModeContext,tokens } from "../../theme"
-import {InputBase} from "@mui/material"
+import InputBase from "@mui/material/InputBase"
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
